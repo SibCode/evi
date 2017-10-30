120996938cbcab0aa5dfd25ed8ce97481f731fb3
Simon Bertschinger <simon.bertschinger@bedag.ch>
2017-10-30 15:34:20 +0100

    Sample register form

---
+++ public_html/register.php
@@ -0,0 +1,33 @@
+<!--
+30.10.2017 by Simon Bertschinger
+This is a simple registry page
+ -->
+<html>
+  <head>
+    <title>
+      Registry Page
+    </title>
+  </head>
+  <body>
+    <h1>
+      Simple Registry Page
+    </h1>
+    <form name="login">
+      Name<input type="text" name="userid"/>
+      Vorname<input type="password" name="pswrd"/>
+      Email<input type="text" name="userid"/>
+      Adresse<input type="password" name="pswrd"/>
+      PLZ<input type="text" name="userid"/>
+      Ort<input type="password" name="pswrd"/>
+      Username<input type="text" name="userid"/>
+      Password<input type="password" name="pswrd"/>
+      <input type="button" onclick="register(this.form)" value="Register"/>
+      <input type="reset" value="Cancel"/>
+    </form>
+    <script language="javascript">
+      function register(form)/*function to check userid & password*/
+      {
+
+    </script>
+  </body>
+</html>
