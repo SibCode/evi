f972b3c9d1f45e8ef5729e6b12b85d9152de3ab8
Kathara <mgreber@gmx.ch>
2017-12-11 12:58:42 +0100

    added DB connection functionality

---
+++ public_html/Functionality/connectDB.php
@@ -0,0 +1,32 @@
+<?php
+
+  $databasehost = "localhost";
+  $databasename = "EVI";
+
+  $databaseusername="root";
+  $databasepassword = "";
+
+  try {
+      $pdo = new PDO("mysql:host=$databasehost;dbname=$databasename",
+          $databaseusername, $databasepassword,
+          array(
+              PDO::MYSQL_ATTR_LOCAL_INFILE => true,
+              PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+          )
+      );
+  } catch (PDOException $e) {
+      die("database connection failed: ".$e->getMessage());
+  }
+
+
+
+  $databasetable = "kriterien";
+
+  $affectedRows = $pdo->exec("
+      LOAD DATA LOCAL INFILE ".$pdo->quote($csvfile)." INTO TABLE `$databasetable`
+        FIELDS TERMINATED BY ".$pdo->quote($fieldseparator)."
+        LINES TERMINATED BY ".$pdo->quote($lineseparator));
+
+  echo "Loaded a total of $affectedRows records from this csv file.\n";
+
+?>
---
+++ public_html/Functionality/initializeDB.php
@@ -0,0 +1,37 @@
+<?php
+
+  include_once("connectDB.php");
+
+  $table = "Kriterien";
+  $fieldseparator = ";";
+  $lineseparator = "\n";
+  $csvfile = "../resources/db/Kriterien.csv";
+
+  $result = $mysqli->query("SHOW TABLES LIKE '".$table."'"));
+
+  if($result->num_rows != 1) {
+    mysqli_query(include_once("../resources/db/initialize.sql"));
+    if(!file_exists($csvfile)) {
+        die("File not found. Make sure you specified the correct path.");
+    }
+    $pdo->exec("
+        LOAD DATA LOCAL INFILE ".$pdo->quote($csvfile)." INTO TABLE `$table`
+          FIELDS TERMINATED BY ".$pdo->quote($fieldseparator)."
+          LINES TERMINATED BY ".$pdo->quote($lineseparator));
+
+  }
+
+
+
+/*
+    $query = <<<eof
+        "LOAD DATA INFILE '$fileName'
+         INTO TABLE tableName
+         FIELDS TERMINATED BY ';'
+         LINES TERMINATED BY '\n'
+        (field1,field2,field3,etc)
+    eof;"
+
+    $db->query($query);
+*/
+?>
